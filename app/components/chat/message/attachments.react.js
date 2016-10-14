3ce101c0c26f16bfdbd8b32697c1e6b222c463bc
Luigi Poole <luigipoole@outlook.com>
2016-10-13 19:04:32 -0700

    attachment Fields & Author (safe)

--- app/components/chat/message/attachments.react.js
+++ app/components/chat/message/attachments.react.js
@@ -9,16 +9,27 @@ export function Thumb({ url }) {
   )
 }
 
-
-export default ({ data: fields }) => { // eslint-disable-line react/prop-types
+export function Fields({ data }) { // eslint-disable-line react/prop-types
   return (
-    <div className='fields'>
-      {fields.map(({short, value, title}, i) => (
-        <div key={title} className={classnames('field', {short})}>
-          <h3>{title || ''}</h3>
-          <p>{value || ''}</p>
+    <div className={styles.fields_container}>
+      {data.map(({short, value = '', title = ''}, i) => (
+        <div key={i+1} className={classnames(styles.field, { [styles.short]: styles })}>
+          <h3>{title}</h3>
+          <p>{value}</p>
         </div>
       ))}
+    </div>
+  )
+}
+
+export function Author({ name, link, image, service = {} }){
+  const { image: serviceImage, name: serviceName } = service
+  return (
+    <div className={styles.author_container}>
+      {serviceImage && <div className={styles.image} style={{backgroundImage: `url(${serviceImage})`}} />}
+      {serviceName && <div className={styles.serviceName}>{serviceName}{name && ' |'}</div>}
+      {image && <div className={styles.image} style={{backgroundImage: `url(${image})`}} />}
+      {name && <div className={styles.name}>{name}</div>}
     </div>
   )
 }
