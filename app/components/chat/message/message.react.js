1a538555bd9179735abc6dd23ffe104c13737ef4
Luigi Poole <luigipoole@outlook.com>
2016-10-13 18:56:05 -0700

    fix edge case where user image was undefined and shit burned

--- app/components/chat/message/message.react.js
+++ app/components/chat/message/message.react.js
@@ -6,7 +6,7 @@ import classnames from 'classnames'
 import styles from 'styles/partials/chat/message/message.scss'
 
 function mapStateToProps({ messages }, { team, channelorDMID, index }) {
-  messages = get(messages, `${team}.${channelorDMID}.messages`)
+  messages = get(messages, `${team}.${channelorDMID}.messages`, [])
   const message = messages[index]
   let firstInChain = index === 0
   if (!firstInChain) {
@@ -33,15 +33,20 @@ export default class Message extends Component {
     users: PropTypes.object.isRequired
   }
 
+  get user() {
+    const { props: { user, userProfile }, context: { users } } = this
+    return userProfile || users[user] || {}
+  }
+
   render() {
-    const { style, firstInChain, userProfile, friendlyTimestamp, text, attachments, user: userID, sendingID } = this.props
-    const user = userProfile || this.context.users[userID]
+    const { style, firstInChain, friendlyTimestamp, text, attachments, sendingID } = this.props
+    const { user } = this
     const className = classnames(styles.message_container, {[styles.firstInChain]: firstInChain}, {[styles.sending]: sendingID})
     return (
       <div className={className} style={style}>
-        <Aside user={user} firstInChain={firstInChain} friendlyTimestamp={friendlyTimestamp} />
+        <Aside {...user} firstInChain={firstInChain} friendlyTimestamp={friendlyTimestamp} />
         <div className={styles.body}>
-          {firstInChain && <Info user={user} friendlyTimestamp={friendlyTimestamp} />}
+          {firstInChain && <Info {...user} friendlyTimestamp={friendlyTimestamp} />}
           {text && <div className={styles.text}>{text}</div>}
         </div>
       </div>
@@ -49,19 +54,19 @@ export default class Message extends Component {
   }
 }
 
-function Info({ user, friendlyTimestamp }){
+function Info({ handle, friendlyTimestamp }){
   return (
     <div className={styles.info}>
-      <span className={styles.user}>{user.handle}</span>
+      <span className={styles.user}>{handle}</span>
       <span className={styles.time}>{friendlyTimestamp}</span>
     </div>
   )
 }
 
-function Aside({ firstInChain, user, friendlyTimestamp }){
+function Aside({ firstInChain, image, images = [], friendlyTimestamp }){
   return (
     <div className={styles.aside}>
-      {firstInChain ? <div style={{backgroundImage: `url(${user.image || user.images[0]})`}} className={styles.profile_pic} /> : (
+      {firstInChain ? <div style={{backgroundImage: `url(${image || images[0]})`}} className={styles.profile_pic} /> : (
         <span className={styles.time}>
           {friendlyTimestamp}
         </span>
